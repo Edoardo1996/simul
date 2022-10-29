ec44fb671d4a6434102ec9a303b01b26e807f460
Edoardo Nervo <edoardonervo96@gmail.com>
2022-10-29 13:18:26 +0200

    Finished Vector lib, add math dir for reference

--- lin-alg/Vector.hpp
+++ lin-alg/Vector.hpp
@@ -11,6 +11,25 @@ public:
     ~Vector(); // Destructor
 
     int GetSize() const;
-    double& Vector::operator[](int i);
+    double Read(int i) const;
+    double CalculateNorm(int p=2) const;  // p-norm
+    void CheckIndex(int i); // Error handling
+    
+    double& Vector::operator[](int i); // Zero-based indexing
+    double& Vector::operator()(int i); // One-based indexing
+    Vector& operator=(const Vector& otherVector);
+    Vector operator+() const;
+    Vector operator-() const;
+    Vector operator+(const Vector& otherVector) const;
+    Vector operator-(const Vector& otherVector) const;
+    // Scalar multiplication
+    Vector operator*(double n) const;
+    // insetion operator
+    friend std::ostream& operator<<(std::ostream& output, const Vector &v);
+   
+    // length function
+    friend int length(const Vector& v);
+    friend double ScalarProduct(const Vector& v1, const Vector& v2);
+    
 
 };
