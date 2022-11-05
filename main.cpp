cbdd94bdbe9ac4eb84a5a793b1c7ba6073447a88
Edoardo Nervo <edoardonervo96@gmail.com>
2022-11-05 12:57:31 +0100

    Add Matrix class

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "Vector.hpp"
+#include "Matrix.hpp"
+
 
 int main()
 { 
     Vector v(3);
+    Matrix m(2, 2);
     v[0] = 1;
     std::cout << v << std::endl;
+    std::cout << m << std::endl;
     return 0;
 }
